927fdf5b2c7799bf454ad1dd030b5acfdfd942a0
Tim254 <tim.mailu@gmail.com>
2022-09-12 03:20:17 +0300

    create relational tables

--- db/migrate/20220912000535_add_authors.rb
+++ db/migrate/20220912000535_add_authors.rb
@@ -1,4 +1,6 @@
 class AddAuthors < ActiveRecord::Migration[6.1]
   def change
+    create_table :authors do |t|
+      t.string :name
   end
 end
--- db/migrate/20220912000549_add_notes.rb
+++ db/migrate/20220912000549_add_notes.rb
@@ -1,4 +1,8 @@
 class AddNotes < ActiveRecord::Migration[6.1]
   def change
+    create_table :notes do |t|
+      t.string :title
+      t.datetime :updated
+      t.string :title
   end
 end
