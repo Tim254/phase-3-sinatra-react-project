97ce8617947537f6ab3ded0678dd6d931cb85ab4
Tim254 <tim.mailu@gmail.com>
2022-09-12 23:17:23 +0300

    specifying ruby version for deployments

--- db/seeds.rb
+++
@@ -1,4 +0,0 @@
-#create pet instances
-garfield = CreatePets.new(name: "garfield", type: "cat", nutrition: "protein", feedings: "3x a day", notes: "Difficulty feeding")
-bosco = CreatePets.new(name: "bosco", type: "dog", nutrition: "protein", feedings: "3x a day", notes: "Prefers red meat")
-hammie = CreatePets.new(name: "hammie", type: "hamster", nutrition: "greens", feedings: "6x a day", notes: "No abnormal feeding habits")
